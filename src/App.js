6c98ae20061c5dbbd275d9b88ddcdbccf4bf4f1c
Yobato <satriyobagus30077@gmail.com>
2022-04-04 23:22:39 +0700

    ini bootstrapnya react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,118 @@
-import logo from './logo.svg';
+// import Hello from './Hello';
+// import Header from './components/Header';
+import React, {useState} from 'react';
+import {
+  Button,
+  Col,
+  Row
+} from 'reactstrap';
+import {
+  Header,
+  NewComponent
+} from './components/Header';
 import './App.css';
 
+// function NewComponent() {
+//   return <div>ini New Component</div>;
+// }
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // const name = 'Nyoman';
+
+  const [name, setName] = useState('Nyoman');
+  const [alamat, setAlamat] = useState('Bali');
+  const [umur] = useState(23);
+
+  const [loginData, setLoginData] = useState({
+    email: null,
+    password: null,
+  });
+
+  const changeNama = (event) => {
+    console.log(event);
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(e);
+  };
+
+  return(
+    <>
+    <Header
+        nama={name}
+        alamat={alamat}
+        umur={26}
+        gantiNama={(e) => setName(e.target.value)}
+        text={<h1>Ini tg html di dalam property/attribute</h1>}
+      />
+      <NewComponent/>
+      <h1>Hello {name}</h1>
+       <form onSubmit={handleSubmit}>
+         <input
+          type="email"
+          placeholder="email"
+          value={loginData.email}
+          onChange={(e) =>
+            setLoginData((prev) => ({ ...prev, email: e.target.value }))
+          }
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={loginData.password}
+          onChange={(e) =>
+            setLoginData((prev) => ({ ...prev, password: e.target.value }))
+          }
+        />
+        <Button type="submit" color="primary" outline>
+          submit
+        </Button>
+       </form>
+       <Row>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+        <Col md={1} style={{ border: '1px solid tomato' }}>
+          <p>Test</p>
+        </Col>
+      </Row>
+    </>
   );
 }
 
